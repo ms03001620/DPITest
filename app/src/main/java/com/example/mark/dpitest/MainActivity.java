5aa55f0b379e0301a8e7319d156b1fe1ca503dcd
Mark <mark@supereffective.com>
2016-01-22 14:45:27 +0800

    Initial commit

---
+++ app/src/main/java/com/example/mark/dpitest/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.mark.dpitest;
+
+import android.content.res.Resources;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView tx = (TextView)findViewById(R.id.text_px);
+        final TextView ta = (TextView)findViewById(R.id.text_area);
+
+
+        float width10 = getResources().getDimension(R.dimen.width_10);
+
+        tx.setText("10dp = "+width10+"px");
+
+        SeekBar seek = (SeekBar)findViewById(R.id.seek);
+        seek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                Log.v("MainActivity", "progress:"+progress);
+
+                Resources r = getResources();
+                float px = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, progress, r.getDisplayMetrics());
+
+                ViewGroup.LayoutParams params = ta.getLayoutParams();
+                ta.setText(progress+"dp");
+                params.height = (int)px;
+                params.width = (int)px;
+                ta.setLayoutParams(params);
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
+    }
+}
