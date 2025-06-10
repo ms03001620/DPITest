230e26868eb357d51d26d625b404e81854687589
zhenjin.ma <zhenjin.ma@linktivity.co.jp>
2025-06-10 10:26:21 +0900

    kotlin; night mode

--- app/src/main/java/com/example/mark/dpitest/MainActivity.kt
+++ app/src/main/java/com/example/mark/dpitest/MainActivity.kt
@@ -1,86 +1,97 @@
-package com.example.mark.dpitest;
+package com.example.mark.dpitest
 
-import android.content.Context;
-import android.content.res.Resources;
-import android.hardware.display.DisplayManager;
-import android.os.Build;
-import android.os.Bundle;
-import android.util.DisplayMetrics;
-import android.util.Log;
-import android.util.TypedValue;
-import android.view.Display;
-import android.view.ViewGroup;
-import android.widget.SeekBar;
-import android.widget.TextView;
+import android.content.res.Configuration
+import android.hardware.display.DisplayManager
+import android.os.Build
+import android.os.Bundle
+import android.util.DisplayMetrics
+import android.util.Log
+import android.util.TypedValue
+import android.view.Display
+import android.view.View
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
-import androidx.appcompat.app.AppCompatActivity;
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
+        val tx = findViewById<View>(R.id.text_px) as TextView
+        val ta = findViewById<View>(R.id.text_area) as TextView
+        val dimenvalue = findViewById<View>(R.id.textvalue) as TextView
 
-public class MainActivity extends AppCompatActivity {
+        dimenvalue.text = makeInfo()
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        val width10 = resources.getDimension(R.dimen.width_10)
 
-        TextView tx = (TextView)findViewById(R.id.text_px);
-        final TextView ta = (TextView)findViewById(R.id.text_area);
-        TextView dimenvalue = (TextView)findViewById(R.id.textvalue);
-        //好山好水好寂寞。真脏真乱真快活
-        dimenvalue.setText("count X:" + getResources().getInteger(R.integer.main_chat_placeholder_critical)
-                + "\n" + getScreenPx(getWindowManager().getDefaultDisplay())
-                + "\n" + getMultiDisplayInfo()
-        );
+        tx.text =
+            "10dp = " + width10 + "px" + ",INT:" + (Build.VERSION.SDK_INT) + ",android:" + Build.VERSION.RELEASE
 
-        float width10 = getResources().getDimension(R.dimen.width_10);
+        val seek = findViewById<View>(R.id.seek) as SeekBar
+        seek.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                Log.v("MainActivity", "progress:$progress")
 
-        tx.setText("10dp = "+width10+"px"+",INT:"+(Build.VERSION.SDK_INT)+",android:"+Build.VERSION.RELEASE);
+                val r = resources
+                val px = TypedValue.applyDimension(
+                    TypedValue.COMPLEX_UNIT_DIP,
+                    progress.toFloat(),
+                    r.displayMetrics
+                )
 
-        SeekBar seek = (SeekBar)findViewById(R.id.seek);
-        seek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                Log.v("MainActivity", "progress:"+progress);
-
-                Resources r = getResources();
-                float px = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, progress, r.getDisplayMetrics());
-
-                ViewGroup.LayoutParams params = ta.getLayoutParams();
-                ta.setText(progress+"dp");
-                params.height = (int)px;
-                params.width = (int)px;
-                ta.setLayoutParams(params);
-
+                val params = ta.layoutParams
+                ta.text = progress.toString() + "dp"
+                params.height = px.toInt()
+                params.width = px.toInt()
+                ta.layoutParams = params
             }
 
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
             }
 
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
             }
-        });
+        })
 
         //getMultiDisplayInfo();
     }
 
-    public String getScreenPx(Display display) {
-        DisplayMetrics displayMetrics = new DisplayMetrics();
-        display.getMetrics(displayMetrics);
-        int height = displayMetrics.heightPixels;
-        int width = displayMetrics.widthPixels;
-        return displayMetrics.toString();
+    fun makeInfo(): String{
+        val sb = StringBuilder()
+
+        sb.append("count X:${resources.getInteger(R.integer.main_chat_placeholder_critical)}")
+        sb.append("${getScreenPx(windowManager.defaultDisplay)}")
+        sb.append("${multiDisplayInfo}")
+        sb.append("isNight:${isNightMode()}")
+
+        return sb.toString()
     }
 
-    public String getMultiDisplayInfo() {
-        String result = "";
-        DisplayManager manager = (DisplayManager) getSystemService(Context.DISPLAY_SERVICE);
-        for (Display display : manager.getDisplays()) {
-            result += getScreenPx(display)+"\n";
+    fun getScreenPx(display: Display): String {
+        val displayMetrics = DisplayMetrics()
+        display.getMetrics(displayMetrics)
+        val height = displayMetrics.heightPixels
+        val width = displayMetrics.widthPixels
+        return displayMetrics.toString()
+    }
+
+    val multiDisplayInfo: String
+        get() {
+            var result = ""
+            val manager =
+                getSystemService(DISPLAY_SERVICE) as DisplayManager
+            for (display in manager.displays) {
+                result += getScreenPx(display) + "\n"
+            }
+            return result
         }
-        return result;
+
+
+    private fun isNightMode(): Boolean {
+        val currentNightMode = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+        return currentNightMode == Configuration.UI_MODE_NIGHT_YES
     }
 }
